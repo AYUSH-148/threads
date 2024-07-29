f5f7d97b80611ac7cd7e091364eb0e576520982a
Ayush <ayushpro111@gmail.com>
2024-07-30 01:28:12 +0530

    fixes in config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript:{
-    ignoreBuildErrors:true
+  typescript: {
+    ignoreBuildErrors: true
   },
-    experimental:{
-        serverComponentsExternalPackages:["mongoose"],
-        serverActions: {
-          bodySizeLimit: '10mb',
-        },
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+    serverActions: {
+      bodySizeLimit: '10mb',
     },
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "img.clerk.com",
-          },
-          {
-            protocol: "https",
-            hostname: "images.clerk.dev",
-          },
-          {
-            protocol: "https",
-            hostname: "uploadthing.com",
-          },
-          {
-            protocol: "https",
-            hostname: "placehold.co",
-          },
-          {
-            protocol: "https",
-            hostname: "utfs.io", // Add the utfs.io hostname here
-          },
-        ],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
-      
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io", // Add the utfs.io hostname here
+      },
+    ],
+  },
+
 };
 
 export default nextConfig;
