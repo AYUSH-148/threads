91df59fef6a09a1c4c2d727129991a406e4e6292
Ayush <ayushpro111@gmail.com>
2024-07-31 00:53:24 +0530

    small fix

--- src/lib/actions/community.actions.ts
+++ src/lib/actions/community.actions.ts
@@ -93,16 +93,12 @@ export async function fetchCommunityPosts(id: string) {
     try {
       connectToDb();
   
-      // Calculate the number of communities to skip based on the page number and page size.
       const skipAmount = (pageNumber - 1) * pageSize;
   
-      // Create a case-insensitive regular expression for the provided search string.
       const regex = new RegExp(searchString, "i");
   
-      // Create an initial query object to filter communities.
       const query: FilterQuery<typeof Community> = {};
   
-      // If the search string is not empty, add the $or operator to match either username or name fields.
       if (searchString.trim() !== "") {
         query.$or = [
           { username: { $regex: regex } },
@@ -110,22 +106,18 @@ export async function fetchCommunityPosts(id: string) {
         ];
       }
   
-      // Define the sort options for the fetched communities based on createdAt field and provided sort order.
       const sortOptions = { createdAt: sortBy };
   
-      // Create a query to fetch the communities based on the search and sort criteria.
       const communitiesQuery = Community.find(query)
         .sort(sortOptions)
         .skip(skipAmount)
         .limit(pageSize)
         .populate("members");
   
-      // Count the total number of communities that match the search criteria (without pagination).
       const totalCommunitiesCount = await Community.countDocuments(query);
   
       const communities = await communitiesQuery.exec();
   
-      // Check if there are more communities beyond the current page.
       const isNext = totalCommunitiesCount > skipAmount + communities.length;
   
       return { communities, isNext };
